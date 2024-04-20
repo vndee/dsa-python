d5ccba83b25336b7ef7418e8a5278bbec3c2b750
Duy Huynh <vndee.huynh@gmail.com>
2024-04-20 13:28:48 +0700

    update: add several solution

---
+++ linear_iteration/two_sum_ii_input_array_is_sorted.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+"""
+Two Sum II - Input array is sorted - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+Approach: Use a dictionary to store the index of the elements. Traverse the array and if the difference between the target
+and the current element is in the dictionary, then return the indices of the elements.
+Time complexity: O(n)
+Space complexity: O(n)
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        d = {}
+        for i, e in enumerate(numbers):
+            if target - e in d:
+                return [d[target - e] + 1, i + 1]
+
+            d[e] = i
---
+++ search/find_first_and_last_position_of_element_in_sorted_array.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+"""
+Find First and Last Position of Element in Sorted Array - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+Approach: Use binary search to find the leftmost and rightmost indices of the target element. Initialize the left and right pointers
+to 0 and n - 1 respectively. While the left pointer is less than or equal to the right pointer, find the middle index. If the
+element at the middle index is less than the target, then update the left pointer to the middle index + 1. If the element at the
+middle index is greater than the target, then update the right pointer to the middle index - 1. If the element at the middle index
+is equal to the target, then update the right pointer to the middle index - 1. If the left pointer is less than n and the element
+at the left pointer is equal to the target, then update the leftmost index to the left pointer. If the right pointer is greater
+than or equal to 0 and the element at the right pointer is equal to the target, then update the rightmost index to the right pointer.
+Return the leftmost and rightmost indices.
+Time complexity: O(log n)
+Space complexity: O(1)
+"""
+
+from typing import List
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        L, R = -1, -1
+        n = len(nums)
+
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) >> 1
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                r = m - 1
+
+        if l < n and nums[l] == target:
+            L = l
+
+        l, r = 0, n - 1
+        while l <= r:
+            m = (l + r) >> 1
+            if nums[m] < target:
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        if r >= 0 and nums[r] == target:
+            R = r
+
+        return [L, R]
